5992ed71f2e4b8bba8f230f41f63da0a95f7d90f
LeZwart <192443@novacollege.nl>
2024-06-12 09:44:43 +0200

    small changes

--- resources/views/News/index.blade.php
+++ resources/views/News/index.blade.php
@@ -1,6 +1,22 @@
+<?php
+
+$search = request('search');
+
+?>
+
 <x-app-layout>
-    <div class="flex row">
-        <a href="{{ route('news.create') }}" class="px-4 py-2 m-2 bg-blue-500 text-white rounded hover:bg-blue-600">Plaats Nieuws</a>
+    <div class="flex row justify-between">
+        <div>
+            <form action="{{ route('news.index') }}" method="GET">
+                <div class="flex row">
+                    <input type="text" name="search" class="px-4 py-2 m-2 bg-white border border-gray-300 rounded shadow focus:outline-none focus:ring focus:border-blue-300" placeholder="Zoekterm">
+                    <button type="submit" class="px-4 py-2 m-2 bg-blue-500 text-white rounded hover:bg-blue-600">Zoeken</button>
+                </div>
+            </form>
+        </div>
+        <div class="flex row">
+            <a href="{{ route('news.create') }}" class="px-4 py-2 m-2 bg-blue-500 text-white rounded hover:bg-blue-600">Plaats Nieuws</a>
+        </div>
     </div>
     @foreach ($newsposts as $news)
         <a href="{{ route('news.show', $news->id) }}" class="block p-4 sm:p-8 bg-white shadow sm:rounded-lg hover:bg-gray-100">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,16 +26,16 @@
                     @if (Route::has('login'))
                         <nav class="flex space-x-4">
                             @auth
-                                <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]">
+                                <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] bg-orange-400 p-5">
                                     Dashboard
                                 </a>
                             @else
-                                <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]">
+                                <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] bg-orange-400 p-5">
                                     Log in
                                 </a>
 
                                 @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]">
+                                    <a href="{{ route('register') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] bg-orange-400 p-5">
                                         Register
                                     </a>
                                 @endif
